32c36dacea1eb1b05e8d8caa2d45be594b5186f3
SpiderBard <chrisnicdao98@gmail.com>
2019-12-11 19:18:08 -0500

    Essentially adds in my php code

---
+++ src/aboutus.php
@@ -0,0 +1,3 @@
+<p> We're a group of students doing this for a webdev project. </br> 
+
+PHP programmer: Christian Nicdao - The architect who started writing the code. </p>
---
+++ src/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang ="eng">
+  <head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  <title>The Bar Crawler Gameplanner</title>
+  </head>
+	
+<body>
+<?php
+include 'header.php';
+?>
+</body>	
+</html>
